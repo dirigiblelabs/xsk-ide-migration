396d30066278394a94092ec5050ff2ed6a1d046b
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2021-07-19 15:11:16 +0300

    Get files list for du packages

--- server/migration/hana-repository.js
+++ server/migration/hana-repository.js
@@ -34,6 +34,221 @@ class HanaRepository{
         this._executeRequest(repositoryRequest, processResponse);
     }
 
+    _getAllPackagesForDu(deliveryUnit, callback){
+        var hanaRepositoryInstance = this;
+
+        hanaRepositoryInstance._listPackages(deliveryUnit, function(error, packages){
+            if(error){
+                return callback(error);
+            }
+            
+            var flatPackages = [].concat.apply([], packages);
+            hanaRepositoryInstance._convertPackagesToRepositoryPackages(flatPackages, callback);
+        });
+
+    }
+
+    _listPackages(du, callback){
+
+        var requestMethod = {
+            action: 'list',
+            what: 'packages',
+            delivery_unit: du.name
+        };
+
+        // var repositoryRequest = new RepositoryRequest(1, requestMethod);
+        var repositoryRequest = requestMethod;
+
+        var processResponse = function(error, response) {
+            if(error){
+                return callback(error);
+            }
+            callback(null, response.content.packages);
+        };
+
+        this._executeRequest(repositoryRequest, processResponse);
+    }
+
+    _convertPackagesToRepositoryPackages(packages, callback){
+        //This needs to be done because the repo-api names the package-name as "package" while halm refers to it as "packageName"
+        var repositoryPackages = [];
+
+        for(let i = 0; i < packages.length; i++) {
+            let pkg = packages[i];
+            var repositoryPackage = new RepositoryPackage(pkg);
+            repositoryPackages.push(repositoryPackage);
+        }
+        callback(null, repositoryPackages);
+
+    }
+
+    _getAllObjectsForPackages(packages, callback){
+        var hanaRepositoryInstance = this;
+
+        let done = 0;
+        for(let i = 0; i < packages.length; i++) {
+            const pkg = packages[i];
+            hanaRepositoryInstance._listObjects(pkg, function(error, objects){
+                // if(error){
+                //     return processingFinished(error);
+                // }
+                
+                done++;
+                if (done === packages.length) {
+                    var flatObjects = [].concat.apply([], objects);
+                    
+                    return callback(null, packages, flatObjects);
+                }
+            });
+
+        }
+
+    }
+
+    _listObjects(pkg, callback){
+        var requestMethod = {
+            action: 'list',
+            what: 'objects',
+            'package': pkg.packageName
+        };
+
+        // console.trace('Starting List objects for package: ' + pkg.packageName);
+
+        var repositoryRequest = requestMethod;
+        var hanaRepositoryInstance = this;
+
+        var processResponse = function(error, response) {
+            //console.trace('List objects finished for package: ' + pkg.packageName + ' #objects: ' + response.content.objects.length);
+            if(error){
+                return callback(error);
+            }
+            
+            hanaRepositoryInstance._convertObjectsToRepositoryObjects(response.content.objects, callback);
+
+        };
+
+        this._executeRequest(repositoryRequest, processResponse);
+    }
+
+    _convertObjectsToRepositoryObjects(objects, callback){
+
+        var hanaRepositoryInstance = this;
+
+        let repositoryObjects = [];
+        for(let i = 0; i < objects.length; i++) {
+            const object = objects[i];
+            var repositoryObject = new RepositoryObject(object.name, object.package, object.suffix);
+            
+            hanaRepositoryInstance._addLanguageAndContent(repositoryObject, (err, result) => {
+                
+                repositoryObjects.push(result);
+                
+                if (repositoryObjects.length === objects.length) {
+                    return callback(null, repositoryObjects);
+                }
+            });
+        }
+
+    }
+
+    _addLanguageAndContent(repositoryObject, callback){
+        var hanaRepositoryInstance = this;
+
+        //logUtil.trace('Loading language and content for: ' + repositoryObject.fullName);
+        
+        hanaRepositoryInstance._getOriginalLanguage(repositoryObject.PackageName.packageName, originalLanguage => {
+            hanaRepositoryInstance._getFileContent(repositoryObject, content => {
+                repositoryObject.originalLanguage = originalLanguage;
+                repositoryObject.content = content;
+
+                return callback(null, repositoryObject);
+            });
+        });
+
+    }
+
+    _getOriginalLanguage(packageName, callback){
+
+        var requestMethod = {
+            action: 'read',
+            what: 'package',
+            'package': packageName
+        };
+
+        var repositoryRequest = requestMethod;
+
+        var processResponse = function(error, response) {
+            if(error){
+                return callback(error);
+            }
+            callback(null, response.content.orig_lang);
+        };
+
+        this._executeRequest(repositoryRequest, processResponse);
+
+    }
+
+
+    _getFileContent(repositoryObject, callback){
+        var requestMethod = {
+            action: "read",
+            what: "object",
+            object: {
+                'package': repositoryObject.PackageName.packageName,
+                name: repositoryObject.simpleName,
+                suffix: repositoryObject.suffix
+            }
+        };
+
+        var repositoryRequest = requestMethod;
+
+        var processResponse = function(error, response) {
+            if(error){
+                return callback(error);
+            }
+            var fileContent;
+
+            if(response.attachments[0].length == 0 && response.attachments[1].length > 0){
+                fileContent = response.attachments[1];
+            } else {
+                fileContent = response.attachments[0];
+            }
+
+            callback(null, fileContent);
+        };
+
+        this._executeRequest(repositoryRequest, processResponse);
+
+    }
+
+    _packagesCollected(packages, fileList, outerCallback) {
+        let deduped = new Map();
+
+        fileList.forEach(function (file) {
+            deduped.set(file._packageName + '::' + file._name + '::' + file._suffix, file);
+        });
+
+        let dedupedList = Array.from(deduped.values());
+        // let filteredList = ObjectTypeFilter.filterObjects(globalContext.includedObjectTypes, globalContext.excludedObjectTypes, dedupedList);
+        outerCallback(null, dedupedList, packages)
+    }
+
+    getAllFilesForDu(globalContext, deliveryUnit, callback){
+
+        var hanaRepositoryInstance = this;
+
+        this._getAllPackagesForDu(deliveryUnit, (err, packages) => {
+   
+            var filteredPackages = packageFilter.filterPackages(globalContext, packages) || [];
+     
+            hanaRepositoryInstance._getAllObjectsForPackages(filteredPackages, (err, packages, fileList) => {
+
+                
+                return this._packagesCollected(packages, fileList, callback);
+            });
+        });
+    }
+
     _executeRequest(repositoryRequest, outerCallback){
     
         var statement = this.hdbClient.prepareCall("CALL SYS.REPOSITORY_REST(?, ?)");
--- server/migration/migrate.js
+++ server/migration/migrate.js
@@ -1,5 +1,6 @@
 let exec = require("core/v4/exec");
 let HanaRepository = require('migration/hana-repository');
+var RepositoryResponse = require('migration/repository-response');
 
 let canonicalPrefix = "/usr/local/tomcat/target/dirigible/repository/root/users/dirigible/workspace/"
 let neoPath = "migration/neo.sh";
@@ -17,17 +18,25 @@ const script = `bash ${canonicalPrefix + neoPath} -a "${account}" -h "${host}" -
 let response = exec.exec(script, {"NEO_CLIENT_PATH": canonicalPrefix + "neo/tools/neo.sh"});
 
 let neoCredentials = JSON.parse(response.substring(response.indexOf("{")));
+
 var database = require("db/v4/database");
 
 database.createDataSource(db, "com.sap.db.jdbc.Driver", neoCredentials['url'], cuser, hanaPass, null);
 
 var connection = database.getConnection('dynamic', db);
 
+let utf8 = org.eclipse.dirigible.api.v3.utils.UTF8Facade;
+let context = {};
 try {
 
     let repo = new HanaRepository(connection);
     repo.getAllDeliveryUnits(function(error, res){
-        console.log("Delivery units fetched: " + res.length);
+        // console.log("Delivery units fetched: " + JSON.stringify(res));
+        const du = {"ach":"","caption":"","lastUpdate":"2021-06-18 11:47:41.1100000","name":"MIGR_TOOLS","ppmsID":"","responsible":"","sp_PPMS_ID":"","vendor":"migration.sap.com","version":"","version_patch":"","version_sp":""};
+        
+        repo.getAllFilesForDu(context, du, (err, files, packages) => {
+            console.log("Files list: " + JSON.stringify(files));
+        })
     })
 
 
