85f8a59365c5672e42fa9730a899a66a061b4e72
Dermendzhiev, Teodor (external - Project) <tdermendjievft@gmail.com>
2021-08-13 12:08:23 +0300

    Get neo script paths from context

--- server/migration/controllers/tunnel-controller.js
+++ server/migration/controllers/tunnel-controller.js
@@ -1,6 +1,8 @@
 const exec = require("core/v4/exec");
 const canonicalPrefix = "/usr/local/tomcat/target/dirigible/repository/root/users/dirigible/workspace/"
-const neoPath = "xsk-ide-migration/server/migration/neo.sh";
+
+const neoPath = __context.get("__neo_path") || canonicalPrefix + "xsk-ide-migration/server/migration/neo.sh";
+const neoClientPath = __context.get("__neo_client_path") || "/usr/local/tomcat/migration-tools/neo/neo-sdk/tools/neo.sh"
 
 class TunnelController {
 
@@ -11,9 +13,9 @@ class TunnelController {
         const password = credentials.password;
         const db = credentials.db;
 
-        const script = `bash ${canonicalPrefix + neoPath} -a "${account}" -h "${host}" -u "${user}" -p "${password}" -i "${db}"`;
+        const script = `bash ${neoPath} -a "${account}" -h "${host}" -u "${user}" -p "${password}" -i "${db}"`;
 
-        const response = exec.exec(script, {"NEO_CLIENT_PATH": "/usr/local/tomcat/migration-tools/neo/neo-sdk/tools/neo.sh"});
+        const response = exec.exec(script, {"NEO_CLIENT_PATH": neoClientPath});
         console.log(response)
         const neoCredentials = JSON.parse(response.substring(response.indexOf("{")));
 
