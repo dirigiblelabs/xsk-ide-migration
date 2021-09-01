709894d2b118ff59fc959ce7b8e9c41823f00b09
Vladimir Mutafov <vlado.mutafov@gmail.com>
2021-09-01 17:11:34 +0300

    chore: remove router as not necessary

--- ide-migration/server/app.js
+++ ide-migration/server/app.js
@@ -1,4 +1,12 @@
-const MigrationRouter = require("ide-migration/server/migration/router");
+const rs = require("http/v4/rs");
+const MigrationFacade = require("ide-migration/server/migration/api/migrate-facade");
 
-const router = new MigrationRouter();
-router.start();
+let facade = new MigrationFacade()
+rs.service()
+      .resource("start-process")
+        .post(facade.startProcess)
+      .resource("continue-process")
+        .post(facade.selectDeliveryUnitAndWorkspaceForProcess)
+      .resource("get-process")
+        .post(facade.getProcessState)
+      .execute();
