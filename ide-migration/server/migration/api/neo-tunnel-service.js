21e94f18a703a30843a0e8b172651a4d81094388
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-28 01:30:29 +0200

    feat: remove the java 8 dependency for the neo sdk

--- ide-migration/server/migration/api/neo-tunnel-service.js
+++ ide-migration/server/migration/api/neo-tunnel-service.js
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2021 SAP SE or an SAP affiliate company and XSK contributors
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company and XSK contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Apache License, v2.0
  * which accompanies this distribution, and is available at
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and XSK contributors
+ * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
 const config = require("core/v4/configurations");
@@ -23,10 +23,7 @@ class NeoTunnelService {
   openTunnel(account, host, jwtToken, databaseId) {
     const script = `${neoClientPath} open-db-tunnel -a "${account}" -h "${host}" -u JWT -p "${jwtToken}" -i "${databaseId}" --output json --background`;
 
-    const rawCommandResult = this.migrationToolExecutor.execute(script, {
-      "JAVA_HOME": config.get("JAVA8_HOME"),
-      "PATH": config.get("JAVA8_HOME") + "/bin:" + config.get("PATH")
-    });
+    const rawCommandResult = this.migrationToolExecutor.execute(script);
 
     const commandResult = JSON.parse(rawCommandResult);
     console.log(commandResult)
@@ -39,10 +36,7 @@ class NeoTunnelService {
 
   closeTunnel(sessionId) {
     const script = `${neoClientPath} close-db-tunnel --session-id ${sessionId}`;
-    this.migrationToolExecutor.execute(script, {
-      "JAVA_HOME": config.get("JAVA8_HOME"),
-      "PATH": config.get("JAVA8_HOME") + "/bin:" + config.get("PATH")
-    });
+    this.migrationToolExecutor.execute(script);
   }
 }
 
