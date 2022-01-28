36344c96763f1418c6572c371e1ed1c26f2fe2fd
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-28 18:57:40 +0200

    add not formatted file

--- ide-migration/server/migration/utils.js
+++ ide-migration/server/migration/utils.js
@@ -1,7 +1,7 @@
 class Utils {
-       static arrayCopy(src, srcIndex, dest, destIndex, length) {
-  dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
- }
+  static arrayCopy(src, srcIndex, dest, destIndex, length) {
+    dest.splice(destIndex, length, ...src.slice(srcIndex, srcIndex + length));
+  }
 
   static intToByteArray(int) {
     return org.eclipse.dirigible.api.v3.io.BytesFacade.intToByteArray(
