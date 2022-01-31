536b56b296073d88e3b5cafea138a2e497eb642a
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-31 23:15:00 +0200

    chore: add eslint in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 npm list -g git-format-staged@2.1.3 || npm install -g git-format-staged@2.1.3
 npm list -g husky@7.0.4 || npm install -g husky@7.0.4
 npm list -g prettier@2.5.1 || npm install -g prettier@2.5.1
+npm list -g eslint@8.8.0 || npm install -g eslint@8.8.0
 
 husky install
