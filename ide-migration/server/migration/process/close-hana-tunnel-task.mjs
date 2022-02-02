b201b93edd9927b1d6bcaa3ac2ec9546e2d5c961
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-02-02 21:29:07 +0200

    fix: prepend class variables with this

--- ide-migration/server/migration/process/close-hana-tunnel-task.mjs
+++ ide-migration/server/migration/process/close-hana-tunnel-task.mjs
@@ -3,26 +3,26 @@ import { TrackService } from "../api/track-service";
 import { NeoTunnelService } from "../api/neo-tunnel-service";
 
 export class CloseHanaTunnelTask {
-    execution = process.getExecutionContext();
+    execution = process.getthis.executionContext();
     trackService = new TrackService();
 
     run() {
         try {
-            process.setVariable(execution.getId(), "migrationState", "TUNNEL_CLOSING");
-            trackService.updateMigrationStatus("TUNNEL_CLOSING");
+            process.setVariable(this.execution.getId(), "migrationState", "TUNNEL_CLOSING");
+            this.trackService.updateMigrationStatus("TUNNEL_CLOSING");
 
-            const userDataJson = process.getVariable(execution.getId(), "userData");
+            const userDataJson = process.getVariable(this.execution.getId(), "userData");
             const userData = JSON.parse(userDataJson);
 
             const neoTunnelService = new NeoTunnelService();
             neoTunnelService.closeTunnel(userData.connectionId);
 
-            process.setVariable(execution.getId(), "migrationState", "TUNNEL_CLOSED");
-            trackService.updateMigrationStatus("TUNNEL CLOSED");
+            process.setVariable(this.execution.getId(), "migrationState", "TUNNEL_CLOSED");
+            this.trackService.updateMigrationStatus("TUNNEL CLOSED");
         } catch (e) {
-            process.setVariable(execution.getId(), "migrationState", "TUNNEL_CLOSING_FAILED");
-            trackService.updateMigrationStatus("TUNNEL CLOSING FAILED");
-            process.setVariable(execution.getId(), "TUNNEL_CLOSING_FAILED_REASON", e.toString());
+            process.setVariable(this.execution.getId(), "migrationState", "TUNNEL_CLOSING_FAILED");
+            this.trackService.updateMigrationStatus("TUNNEL CLOSING FAILED");
+            process.setVariable(this.execution.getId(), "TUNNEL_CLOSING_FAILED_REASON", e.toString());
         }
     }
 }
