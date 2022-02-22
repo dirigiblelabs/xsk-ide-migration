1ee4c0ea7cb2b50809afe90cfa3783e939f1186e
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-02-22 14:22:09 +0200

    feat: introduce flowable sub-processes (#56)
    * feat: add separate flowable subprocesses and refactor error handling and tracking
    
    * chore: address PR comments

--- ide-migration/server/migration/process/close-hana-tunnel-task.mjs
+++ ide-migration/server/migration/process/close-hana-tunnel-task.mjs
@@ -1,28 +1,19 @@
 import { process } from "@dirigible/bpm";
-import { TrackService } from "../api/track-service.mjs";
 import { NeoTunnelService } from "../api/neo-tunnel-service.mjs";
+import { MigrationTask } from "./task.mjs";
 
-export class CloseHanaTunnelTask {
+export class CloseHanaTunnelTask extends MigrationTask {
     execution = process.getExecutionContext();
-    trackService = new TrackService();
+
+    constructor() {
+        super("TUNNEL_CLOSING", "TUNNEL_CLOSED", "TUNNEL_CLOSING_FAILED");
+    }
 
     run() {
-        try {
-            process.setVariable(this.execution.getId(), "migrationState", "TUNNEL_CLOSING");
-            this.trackService.updateMigrationStatus("TUNNEL_CLOSING");
+        const userDataJson = process.getVariable(this.execution.getId(), "userData");
+        const userData = JSON.parse(userDataJson);
 
-            const userDataJson = process.getVariable(this.execution.getId(), "userData");
-            const userData = JSON.parse(userDataJson);
-
-            const neoTunnelService = new NeoTunnelService();
-            neoTunnelService.closeTunnel(userData.connectionId);
-
-            process.setVariable(this.execution.getId(), "migrationState", "TUNNEL_CLOSED");
-            this.trackService.updateMigrationStatus("TUNNEL CLOSED");
-        } catch (e) {
-            process.setVariable(this.execution.getId(), "migrationState", "TUNNEL_CLOSING_FAILED");
-            this.trackService.updateMigrationStatus("TUNNEL CLOSING FAILED");
-            process.setVariable(this.execution.getId(), "TUNNEL_CLOSING_FAILED_REASON", e.toString());
-        }
+        const neoTunnelService = new NeoTunnelService();
+        neoTunnelService.closeTunnel(userData.connectionId);
     }
 }
--- ide-migration/server/migration/process/list-delivery-units-task.mjs
+++ ide-migration/server/migration/process/list-delivery-units-task.mjs
@@ -1,30 +1,23 @@
 import { process } from "@dirigible/bpm";
 import { MigrationService } from "../api/migration-service.mjs";
-import { TrackService } from "../api/track-service.mjs";
+import { MigrationTask } from "./task.mjs";
 
-export class ListDeliveryUnitsTask {
+export class ListDeliveryUnitsTask extends MigrationTask {
     execution = process.getExecutionContext();
-    trackService = new TrackService();
+
+    constructor() {
+        super("DELIVERY_UNITS_LISTING", "DELIVERY_UNITS_LISTED", "DELIVERY_UNITS_LISTING_FAILED");
+    }
 
     run() {
-        try {
-            process.setVariable(this.execution.getId(), "migrationState", "DELIVERY_UNITS_LISTING");
-            this.trackService.updateMigrationStatus("DELIVERY UNITS LISTING");
-            const userDataJson = process.getVariable(this.execution.getId(), "userData");
-            const userData = JSON.parse(userDataJson);
-            const userDatabaseData = userData.hana;
-            const connectionUrl = process.getVariable(this.execution.getId(), "connectionUrl");
+        const userDataJson = process.getVariable(this.execution.getId(), "userData");
+        const userData = JSON.parse(userDataJson);
+        const userDatabaseData = userData.hana;
+        const connectionUrl = process.getVariable(this.execution.getId(), "connectionUrl");
 
-            const migrationService = new MigrationService();
-            migrationService.setupConnection(userDatabaseData.databaseSchema, userDatabaseData.username, userDatabaseData.password, connectionUrl);
-            const deliveryUnits = migrationService.getAllDeliveryUnits();
-            process.setVariable(this.execution.getId(), "deliveryUnits", JSON.stringify(deliveryUnits));
-            process.setVariable(this.execution.getId(), "migrationState", "DELIVERY_UNITS_LISTED");
-            this.trackService.updateMigrationStatus("DELIVERY UNITS LISTED");
-        } catch (e) {
-            process.setVariable(this.execution.getId(), "migrationState", "DELIVERY_UNITS_LISTING_FAILED");
-            this.trackService.updateMigrationStatus("DELIVERY UNITS LISTING_FAILED");
-            process.setVariable(this.execution.getId(), "DELIVERY_UNITS_LISTING_FAILED_REASON", e.toString());
-        }
+        const migrationService = new MigrationService();
+        migrationService.setupConnection(userDatabaseData.databaseSchema, userDatabaseData.username, userDatabaseData.password, connectionUrl);
+        const deliveryUnits = migrationService.getAllDeliveryUnits();
+        process.setVariable(this.execution.getId(), "deliveryUnits", JSON.stringify(deliveryUnits));
     }
 }
