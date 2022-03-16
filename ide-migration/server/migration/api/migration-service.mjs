0ddef0caaeb7f625f972caea3ee2096be455b119
dpanayotov <dpanayotov94@gmail.com>
2022-03-16 16:30:15 +0200

    common function to generate container name

--- ide-migration/server/migration/api/migration-service.mjs
+++ ide-migration/server/migration/api/migration-service.mjs
@@ -80,7 +80,7 @@ export class MigrationService {
 
     createHdiFile(deliveryUnitName, workspaceName, project, hdiConfigPath, deployables) {
         const projectName = project.getName();
-        const groupOrContainerHdiValue = `${deliveryUnitName}_${projectName}`.toUpperCase();
+        const groupOrContainerHdiValue = this._buildCalcViewModel(deliveryUnitName, projectName);
         const defaultHanaUser = config.get(HANA_USERNAME, "DBADMIN");
 
         const hdi = {
