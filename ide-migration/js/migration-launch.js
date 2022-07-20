bb5df8b89b690a465a6812c06ed8916d26df10d2
StanZGenchev <stan.z.genchev@gmail.com>
2022-07-20 11:39:54 +0300

    Updated perspective shell

--- ide-migration/js/migration-launch.js
+++ ide-migration/js/migration-launch.js
@@ -9,31 +9,28 @@
  * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and XSK contributors
  * SPDX-License-Identifier: Apache-2.0
  */
-var migrationLaunchView = angular.module("migration-launch", ["angularFileUpload"]);
-
+let migrationLaunchView = angular.module("migration-launch", ["angularFileUpload"]);
 
 migrationLaunchView.factory("$messageHub", [
     function () {
-        var messageHub = new FramesMessageHub();
-        var announceAlert = function (title, message, type) {
+        let messageHub = new FramesMessageHub();
+        let announceAlert = function (title, message, type) {
             messageHub.post(
                 {
-                    data: {
-                        title: title,
-                        message: message,
-                        type: type,
-                    },
+                    title: title,
+                    message: message,
+                    type: type,
                 },
                 "ide.alert"
             );
         };
-        var announceAlertError = function (title, message) {
+        let announceAlertError = function (title, message) {
             announceAlert(title, message, "error");
         };
-        var message = function (evtName, data) {
+        let message = function (evtName, data) {
             messageHub.post({ data: data }, evtName);
         };
-        var on = function (topic, callback) {
+        let on = function (topic, callback) {
             messageHub.subscribe(callback, topic);
         };
         return {
